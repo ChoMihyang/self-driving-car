3bff465f5ac9a17555c364fcf37a65683756defc
Cho-Mihyang <algid8252@g.yju.ac.kr>
2021-09-26 16:47:51 +0900

    [Lane] 이미지 블러 처리

---
+++ finding_lanes/gaussianBlur.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('../image/TestTrack_1.png', 1)
+image = cv2.resize(image, dsize=(640, 480), interpolation=cv2.INTER_AREA)
+
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY) # 3 Channel -> 1 Channel
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+cv2.imshow("result", blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
