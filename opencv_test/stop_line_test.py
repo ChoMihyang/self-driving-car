892699c90c6fcf22be838d9b3dee446d65435923
Cho-Mihyang <algid8252@g.yju.ac.kr>
2021-11-15 12:50:13 +0900

    [Stop Line] 정지선 인식 알고리즘 진행중

--- opencv_test/stop_line_test.py
+++ opencv_test/stop_line_test.py
@@ -4,7 +4,7 @@ import numpy as np
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) 
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur, 50, 200, None, 3)
+    canny = cv2.Canny(blur, 10, 300, None, 3)
     return canny
 
 def region_of_interest(image):
@@ -13,16 +13,22 @@ def region_of_interest(image):
 
     # 관심 영역 표시 [LB][LT][RT][RB]
     # img_1
-    _shape = np.array([
-        [int(0.11 * w), int(0.65 * h)], [int(0.13 * w), int(0.45 * h)],
-        [int(0.75 * w), int(0.45 * h)], [int(0.77 * w), int(0.65 * h)],
-    ])
+    # _shape = np.array([
+    #     [int(0.11 * w), int(0.65 * h)], [int(0.13 * w), int(0.45 * h)],
+    #     [int(0.75 * w), int(0.45 * h)], [int(0.77 * w), int(0.65 * h)],
+    # ])
 
     # img_2
     # _shape = np.array([
-    #     [int(0.01 * x), int(0.95 * y)], [int(0.01 * x), int(0.75 * y)],
-    #     [int(0.90 * x), int(0.75 * y)], [int(0.90 * x), int(0.95 * y)],
+    #     [int(0.01 * w), int(0.95 * h)], [int(0.01 * w), int(0.75 * h)],
+    #     [int(0.90 * w), int(0.75 * h)], [int(0.90 * w), int(0.95 * h)],
     # ])
+
+    # img_3
+    _shape = np.array([
+        [int(0.01 * w), int(0.95 * h)], [int(0.01 * w), int(0.65 * h)],
+        [int(0.99 * w), int(0.65 * h)], [int(0.99 * w), int(0.95 * h)],
+    ])
    
     mask = np.zeros_like(image)
    
@@ -45,8 +51,9 @@ def display_lines(image, lines):
             cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 10)
     return line_image
 
-img = cv2.imread('../image/stop_line_1.png')
+# img = cv2.imread('../image/stop_line_1.png')
 # img = cv2.imread('../image/stop_line_2.png')
+img = cv2.imread('../image/stop_line_3.png')
 
 while cv2.waitKey(1) != ord('q'):
 
@@ -58,6 +65,6 @@ while cv2.waitKey(1) != ord('q'):
     line_image = display_lines(lane_image, lines)
     combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
     
-    cv2.imshow('result', combo_image)
+    cv2.imshow('result', canny_img)
 
 cv2.destroyAllWindows()
--- opencv_test/stop_line_video_test.py
+++ opencv_test/stop_line_video_test.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
+from numpy.core.numeric import False_
 
 # 이미지 프로세싱
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) 
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur, 50, 200, None, 3)
+    canny = cv2.Canny(blur, 7, 200, None, 3)
     return canny
 
 # 관심 영역 표시
@@ -15,8 +16,8 @@ def region_of_interest(image):
 
     # 관심 영역 표시 [LB][LT][RT][RB]
     _shape = np.array([
-        [int(0.11 * w), int(0.65 * h)], [int(0.13 * w), int(0.45 * h)],
-        [int(0.75 * w), int(0.45 * h)], [int(0.77 * w), int(0.65 * h)],
+        [int(0.01 * w), int(0.95 * h)], [int(0.01 * w), int(0.65 * h)],
+        [int(0.99 * w), int(0.65 * h)], [int(0.99 * w), int(0.95 * h)],
     ])
     
     mask = np.zeros_like(image)
@@ -34,30 +35,52 @@ def region_of_interest(image):
 
 # 선 그리기
 def display_lines(image, lines):
-    global count
     line_image = np.zeros_like(image)
+    detection = False
+    
     if lines is not None:
+        print(type(len(lines)))
         for line in lines:
+
+            if len(lines) > 0 :
+                print('차선 인식 있음')
+                detection = True
+
             x1, y1, x2, y2 = line.reshape(4)
-            # print('x1 : ', x1, ' / y1 : ', y1, ' / x2 : ', x2, ' / y2 : ', y2)
+            print('x1 : ', x1, ' / y1 : ', y1, ' / x2 : ', x2, ' / y2 : ', y2)
             # line(img, 시작 좌표(x1, y1), 종료 좌표(x2, y2), color, 선 두께)
             cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 5) 
+
+    elif lines is None:
+        print('차선 인식 없음')
     
-    return line_image
+    return detection, line_image 
 
 
 ##### 정지선 카운팅 메서드 시작 #####
-stop_line_count = 0
+stop_line_count = 1
 
-def flag():
+def flag(detection):
     global stop_line_count
+    stop_point = True
 
-    return stop_line_count   
+    if detection is True:
+        print(stop_line_count, '바퀴')
+        stop_line_count += 1
+        
+        if stop_line_count == 4:
+            print('주행 완료, 최종 정지')
+            stop_point = False
+
+    detection = False
+
+    return stop_point
+   
 ##### 정지선 카운팅 메서드 끝 #####
 
 
 # 비디오 불러오기
-cap = cv2.VideoCapture("../image/stopline_test_1.mp4")
+cap = cv2.VideoCapture("../image/stopline_test_3.mp4")
 
 while True:
     ret, img = cap.read()
@@ -71,11 +94,13 @@ while True:
     # TODO : 정지 (수평)선 기울기에 따라 𝜃값 변경(0~180)
     # HoughLinesP(이미지, rho[0~1], theta[𝜃], 임계값, 선의 최소 길이, 선사이 최대 허용 간격)
     lines = cv2.HoughLinesP(roi_image, 1, np.pi / 2, 100, np.array([]), minLineLength=40, maxLineGap=5)
-    line_image = display_lines(lane_image, lines)
+    detection, line_image = display_lines(lane_image, lines)
     combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-    
+    stop_point = flag(detection) # 정지선 카운트
+    print("stop_point : ", stop_point)
     cv2.imshow("result", combo_image)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+
+    if cv2.waitKey(25) & 0xFF == ord("q"):
         break
 
 cap.release()
